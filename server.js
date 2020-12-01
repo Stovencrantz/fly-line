eca81f6dfe28603907f661b03c251fcacb20f4bd
Stovencrantz <sknapp143@gmail.com>
2020-12-01 18:42:17 -0500

    Added changes for heroku deployment

--- server.js
+++ server.js
@@ -4,20 +4,28 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
 // API
 const users = require('./api/users');
 app.use(users);
 
-app.use(express.static(path.join(__dirname, './client/build')))
+// app.use(express.static(path.join(__dirname, '/client/build')))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'));
+    res.sendFile(path.join(__dirname, '/client/build'));
 })
+
+
+
 
 const connection = "mongodb+srv://stovencrantz:Copp3rloon!@cluster0.mwxbg.mongodb.net/users?retryWrites=true&w=majority";
 mongoose.connect(connection,
@@ -29,6 +37,6 @@ mongoose.connect(connection,
     .then(() => console.log('Database connected successfully'))
     .catch((err) => console.log(err))
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
