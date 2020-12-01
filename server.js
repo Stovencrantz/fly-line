a93cf10470a4730597d0802008e46ebce60aaeb4
Stovencrantz <sknapp143@gmail.com>
2020-12-01 18:17:36 -0500

    Altered route to build folder in server.js to match the correct route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,9 +14,9 @@ app.use(cors());
 const users = require('./api/users');
 app.use(users);
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'));
+    res.sendFile(path.join(__dirname, './client/build'));
 })
 
 const connection = "mongodb+srv://stovencrantz:Copp3rloon!@cluster0.mwxbg.mongodb.net/users?retryWrites=true&w=majority";
@@ -27,7 +29,6 @@ mongoose.connect(connection,
     .then(() => console.log('Database connected successfully'))
     .catch((err) => console.log(err))
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
