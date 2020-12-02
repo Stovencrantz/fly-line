74967b1f9002138b58a4dce555486b3938282798
Stovencrantz <sknapp143@gmail.com>
2020-12-01 19:06:56 -0500

    correcting build directory path

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(users);
 
 // app.use(express.static(path.join(__dirname, '/client/build')))
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("./client/build"));
   }
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build'));
+    res.sendFile(path.join(__dirname, './client/build'));
 })
 
 
