a2a0495a08e0e61271514d19004e04f3ad23009d
Stovencrantz <sknapp143@gmail.com>
2020-12-07 22:14:41 -0500

    Added text in my server.js file to serve the index.html file if a route is not recognized.

--- server.js
+++ server.js
@@ -19,12 +19,15 @@ app.use(morgan('tiny'));
 const users = require('./routes/usersRoutes');
 app.use(users);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
     app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
   }
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'));
-})
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/users",
     {
