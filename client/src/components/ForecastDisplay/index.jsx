b2589ff7fbdc9f7e4c80005ba0e3cf7b59806a3e
Stovencrantz <sknapp143@gmail.com>
2021-01-06 22:38:39 -0500

    adjusted the current weather display to have an improved grid layout for aligning items

--- client/src/components/ForecastDisplay/index.jsx
+++ client/src/components/ForecastDisplay/index.jsx
@@ -19,7 +19,7 @@ import CloudTwoToneIcon from "@material-ui/icons/CloudTwoTone";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationArrow, faWater } from '@fortawesome/free-solid-svg-icons'
+import { faLocationArrow, faWater } from "@fortawesome/free-solid-svg-icons";
 
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
@@ -63,14 +63,20 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3),
   },
   gridItems: {
-    alignItems: "center"
+    alignItems: "center",
+    height: "100%",
   },
   paper: {
-    // padding: theme.spacing(1),
-    textAlign: "center",
-    justify: "center",
+    padding: theme.spacing(1),
+    justifyContent: "center",
     color: theme.palette.text.secondary,
-    background: theme.palette.success.light,
+    boxShadow: "none",
+    margin: "2px",
+  },
+  typ: {
+    backgroundColor: "white",
+    margin: "5px",
+    textAlign: "center",
     fontSize: "0.7em",
     fontWeight: "bold",
   },
@@ -89,28 +95,49 @@ function ForecastDisplay(props) {
   console.log(iconUrl);
 
   const { maritimeData } = props.maritimeData;
-  console.log("Current maritime data from props", maritimeData)
+  console.log("Current maritime data from props", maritimeData);
 
   function kelvinToFahrenheit(kelvin) {
     let fahrenheit = (kelvin - 273.15) * (9 / 5) + 32;
     return Math.round(fahrenheit);
   }
   function celsiusToFahrenheit(celsius) {
-    let fahrenheit = (celsius*(9/5))+32;
+    let fahrenheit = celsius * (9 / 5) + 32;
     return Math.round(fahrenheit);
   }
 
-  function degToCompass(num) {
-    let val = Math.floor((num / 22.5) + 0.5);
-    let arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-    return arr[(val % 16)];
-}
-
-  function mpsToMph(metersPerSecond) {
-    let milesPerHour = Math.floor(metersPerSecond*2.237);
-    return milesPerHour;
+  function metersToFeet(meters) {
+    let feet = Math.round(meters*3.281);
+    return feet;
   }
 
+  function degToCompass(num) {
+    let val = Math.floor(num / 22.5 + 0.5);
+    let arr = [
+      "N",
+      "NNE",
+      "NE",
+      "ENE",
+      "E",
+      "ESE",
+      "SE",
+      "SSE",
+      "S",
+      "SSW",
+      "SW",
+      "WSW",
+      "W",
+      "WNW",
+      "NW",
+      "NNW",
+    ];
+    return arr[val % 16];
+  }
+
+  function mpsToMph(metersPerSecond) {
+    let milesPerHour = Math.floor(metersPerSecond * 2.237);
+    return milesPerHour;
+  }
 
   // Actual content within our drawer
   const drawer = (
@@ -126,29 +153,41 @@ function ForecastDisplay(props) {
           <ListItemText primary="Current Weather" />
         </ListItem>
         <ListItem>
-          <Grid container xs={12} spacing={1} justify="center"  direction="row">
+          <Grid
+            container
+            xs={12}
+            spacing={1}
+            // justify="space-evenly"
+            // alignItems="center"
+          >
             {/* ====================================================================================== */}
             {/* weather */}
-            {/* ====================================================================================== */}
-
-            <Grid item xs={4}  id="weatherDetails" style={{alignSelf: "center"}}>
-              <Grid container className={classes.gridItems} >
-                <Grid item xs={6} spacing={1} direction="column">
-                  <Paper className={classes.paper}>
+            {/* ====================================================================================== */}{" "}
+            <Grid
+              item
+              xs={4}
+            >
+              <Grid container xs={12}  id="weatherDetails"
+>
+                <Grid
+                  item
+                  xs={6}
+                >
                     <img id="wicon" src={iconUrl} alt="Weather icon" />
-                  </Paper>
                 </Grid>
-                <Grid item xs={6} spacing={1} direction="column">
-                  <Paper className={classes.paper} style={{ fontSize: "1em" }}>
-                    <p>{kelvinToFahrenheit(currentWeatherData.temp)}°</p>
-                  </Paper>
-                </Grid>
-                <Grid item xs={12} spacing={1} direction="column">
+                <Grid item xs={6} >
                   <Paper className={classes.paper}>
-                    <p>
+                    <Typography className={classes.typ}>
+                      {kelvinToFahrenheit(currentWeatherData.temp)}°
+                    </Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} >
+                  <Paper className={classes.paper}>
+                    <Typography className={classes.typ} style={{ marginTop: "26px"}}>
                       Feels Like{" "}
                       {kelvinToFahrenheit(currentWeatherData.feelsLike)}°
-                    </p>
+                    </Typography>
                   </Paper>
                 </Grid>
               </Grid>
@@ -156,13 +195,113 @@ function ForecastDisplay(props) {
             {/* ====================================================================================== */}
             {/* Wind */}
             {/* ====================================================================================== */}
+            <Grid
+              item
+              xs={4}
+            >
+              <Grid container xs={12} >
+                <Grid item xs={6} >
+                  <Paper className={classes.paper}>
+                    <FontAwesomeIcon
+                      icon={faLocationArrow}
+                      size="2x"
+                      style={{
+                        transform: `rotate(${
+                          currentWeatherData.windDirection - 45
+                        }deg)`,
+                      }}
+                    />
+                  </Paper>
+                </Grid>
+                <Grid item xs={6} >
+                  <Paper className={classes.paper}>
+                    <Typography className={classes.typ}>
+                      {mpsToMph(currentWeatherData.windSpeed)}mph
+                    </Typography>
+                    <Typography className={classes.typ}>
+                      {degToCompass(currentWeatherData.windDirection)}
+                    </Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} >
+                  <Paper className={classes.paper}>
+                    <Typography className={classes.typ}>
+                      Gusts up to {mpsToMph(currentWeatherData.windGust)}mph
+                    </Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Grid>
+            {/* maritime grid item */}
+            <Grid
+              item
+              xs={4}
+            >
+              <Grid
+                container
+                xs={12}
+              >
+                <Grid item xs={6} >
+                  <Paper className={classes.paper}>
+                    <FontAwesomeIcon
+                      icon={faWater}
+                      size="2x"
+                      style={{ color: "cyan" }}
+                    />
+                  </Paper>
+                </Grid>
+                <Grid item xs={6} >
+                  <Paper className={classes.paper}>
+                    <Typography className={classes.typ}>
+                      {celsiusToFahrenheit(maritimeData.waterTemp)}°
+                    </Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} >
+                  <Paper className={classes.paper}>
+                    <Typography className={classes.typ} style={{ marginTop: "30px"}}>
+                      {metersToFeet(maritimeData.waveHeight)} feet
+                    </Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* <Grid container xs={12} spacing={1} justify="center"  direction="row"> */}
 
+          {/* ====================================================================================== */}
+          {/* weather */}
+          {/* ====================================================================================== */}
+          {/* 
+            <Grid item xs={4}  id="weatherDetails" style={{alignSelf: "center"}}>
+              <Grid container className={classes.gridItems} >
+                <Grid item xs={6} spacing={1} direction="column">
+                  <Paper className={classes.paper} elevation={0} >
+                    <img id="wicon" src={iconUrl} alt="Weather icon" />
+                  </Paper>
+                </Grid>
+                <Grid item xs={6} spacing={1} direction="column">
+                  <Paper className={classes.paper} style={{ fontSize: "1em" }}>
+                    {kelvinToFahrenheit(currentWeatherData.temp)}°
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} spacing={1} direction="column">
+                  <Paper className={classes.paper}>
+                      Feels Like{" "}{kelvinToFahrenheit(currentWeatherData.feelsLike)}°
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Grid> */}
+          {/* ====================================================================================== */}
+          {/* Wind */}
+          {/* ====================================================================================== */}
+          {/* 
             <Grid item xs={4}  id="windDetails">
               <Grid container className={classes.gridItems}>
                 <Grid item xs={6} spacing={1} direction="column">
-                  <Paper className={classes.paper}>
-                    {/* we subtract 45 degrees from the total angle to correctly align the arrow in our icon with the true angle position*/}
-                    <FontAwesomeIcon icon={faLocationArrow} size="2x" style={{ transform: `rotate(${currentWeatherData.windDirection-45}deg)`}}/>
+                  <Paper className={classes.paper}> */}
+          {/* we subtract 45 degrees from the total angle to correctly align the arrow in our icon with the true angle position*/}
+          {/* <FontAwesomeIcon icon={faLocationArrow} size="2x" style={{ transform: `rotate(${currentWeatherData.windDirection-45}deg)`}}/>
                   </Paper>
                 </Grid>
                 <Grid item xs={6} spacing={1} direction="column">
@@ -171,21 +310,19 @@ function ForecastDisplay(props) {
                   </Paper>
                   <Paper className={classes.paper} style={{ fontSize: "1em" }}>
                     {degToCompass(currentWeatherData.windDirection)}
-
                   </Paper>
                 </Grid>
                 <Grid item xs={12} spacing={1} direction="column">
-                  <Paper className={classes.paper}>
-                    <p>Gusts up to {mpsToMph(currentWeatherData.windGust)}mph
-                    </p>
+                  <Paper className={classes.paper} style={{ marginTop: "14px"}}>
+                    Gusts up to {mpsToMph(currentWeatherData.windGust)}mph
                   </Paper>
                 </Grid>
               </Grid>
-            </Grid>
-            {/* ====================================================================================== */}
-            {/* Waves */}
-            {/* ====================================================================================== */}
-            <Grid item xs={4}  id="waveDetails">
+            </Grid> */}
+          {/* ====================================================================================== */}
+          {/* Waves */}
+          {/* ====================================================================================== */}
+          {/* <Grid item xs={4}  id="waveDetails">
               <Grid container className={classes.gridItems}>
                 <Grid item xs={6} spacing={1} direction="column">
                   <Paper className={classes.paper}>
@@ -194,24 +331,39 @@ function ForecastDisplay(props) {
                 </Grid>
                 <Grid item xs={6} spacing={1} direction="column">
                   <Paper className={classes.paper} style={{ fontSize: "1em" }}>
-                    <p>{celsiusToFahrenheit(maritimeData.waterTemp)}°</p>
+                      {celsiusToFahrenheit(maritimeData.waterTemp)}°
                   </Paper>
                 </Grid>
                 <Grid item xs={12} spacing={1} direction="column">
-                  <Paper className={classes.paper}>
-                    <p>
-                      Feels Like{" "}
-                      {kelvinToFahrenheit(currentWeatherData.feelsLike)}°
-                    </p>
+                  <Paper className={classes.paper} style={{ marginTop: "28px"}}>
+                      Feels Like {kelvinToFahrenheit(currentWeatherData.feelsLike)}°
                   </Paper>
                 </Grid>
               </Grid>
             </Grid>
+          </Grid> */}
 
-            {/* <Grid Container xs={6} spacing={1}>
-              Wind
-            </Grid> */}
-          </Grid>
+          {/* attempt at adding a new row */}
+
+          {/* <Grid container xs={12} spacing={1} justify="center"  direction="row">
+            <Grid item xs={12} spacing={1} direction="column">
+              <Paper className={classes.paper}>
+                  Feels Like{" "}{kelvinToFahrenheit(currentWeatherData.feelsLike)}°
+              </Paper>
+            </Grid>
+
+            <Grid item xs={12} spacing={1} direction="column">
+              <Paper className={classes.paper} style={{ marginTop: "14px"}}>
+                Gusts up to {mpsToMph(currentWeatherData.windGust)}mph
+              </Paper>
+            </Grid>      
+
+            <Grid item xs={12} spacing={1} direction="column">
+              <Paper className={classes.paper} style={{ marginTop: "28px"}}>
+                Feels Like {kelvinToFahrenheit(currentWeatherData.feelsLike)}°
+              </Paper>
+            </Grid>     
+          </Grid>  */}
         </ListItem>
       </List>
       {/* <List>
@@ -224,14 +376,72 @@ function ForecastDisplay(props) {
       </List> */}
       <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button>
+          <Grid
+            container
+            xs={12}
+            spacing={1}
+            justify="space-evenly"
+            alignItems="center"
+            style={{ backgroundColor: "red" }}
+          >
+            {/* weather grid item */}
+            <Grid
+              item
+              xs={4}
+              style={{ backgroundColor: "yellow", textAlign: "center" }}
+            >
+              <Grid container xs={12} style={{ backgroundColor: "orange" }}>
+                <Grid item xs={6} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>6</Typography>
+                </Grid>
+                <Grid item xs={6} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>6</Typography>
+                </Grid>
+                <Grid item xs={12} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>12</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            {/* wind grid item */}
+            <Grid
+              item
+              xs={4}
+              style={{ backgroundColor: "yellow", textAlign: "center" }}
+            >
+              <Grid container xs={12} style={{ backgroundColor: "orange" }}>
+                <Grid item xs={6} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>6</Typography>
+                </Grid>
+                <Grid item xs={6} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>6</Typography>
+                </Grid>
+                <Grid item xs={12} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>12</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            {/* maritime grid item */}
+            <Grid
+              item
+              xs={4}
+              style={{ backgroundColor: "yellow", textAlign: "center" }}
+            >
+              <Grid container xs={12} style={{ backgroundColor: "orange" }}>
+                <Grid item xs={6} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>6</Typography>
+                </Grid>
+                <Grid item xs={6} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>6</Typography>
+                </Grid>
+                <Grid item xs={12} style={{ backgroundColor: "green" }}>
+                  <Typography className={classes.typ}>12</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </ListItem>
       </List>
       <Divider />
       <List>
