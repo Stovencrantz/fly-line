c191d5aa029138efc5105e99b0c7672db96b3f45
Stovencrantz <sknapp143@gmail.com>
2021-01-23 21:59:19 -0500

    Added navigational; button list in post modal. Corrected button layout, position, sizing, and color theme with static vs hover gradients.

--- client/src/components/FooterNav/index.jsx
+++ client/src/components/FooterNav/index.jsx
@@ -3,17 +3,20 @@ import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 // import Link from "@material-ui/core/Link";
-import {Link} from "react-router-dom";
-import Modal from "@material-ui/core/Modal";
+import { Link } from "react-router-dom";
+import { Modal, Button, List, ListItem, ListItemIcon } from "@material-ui/core";
 import FolderIcon from "@material-ui/icons/Folder";
 import RestoreIcon from "@material-ui/icons/Restore";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import MapIcon from "@material-ui/icons/Map";
 import DynamicFeedIcon from "@material-ui/icons/DynamicFeed";
-import AddBoxIcon from '@material-ui/icons/AddBox';
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import PostAddIcon from "@material-ui/icons/PostAdd";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import RadioIcon from "@material-ui/icons/Radio";
 
-import "./style.css"
+import "./style.css";
 
 export default function FooterNav() {
   const useStyles = makeStyles((theme) => ({
@@ -25,13 +28,23 @@ export default function FooterNav() {
       bottom: "0px",
       textAlign: "center",
     },
-
-    // menuButton: {
-    //   marginRight: theme.spacing(2),
-    // },
     title: {
       flexGrow: 1,
     },
+    listItemButton: {
+      marginBottom: '2em',
+      borderRadius: '50px',
+
+      backgroundColor: "slategrey",
+      '&:hover': {
+        backgroundColor: 'darkgrey'
+      },
+      padding: '.75rem',
+    },
+    listItemIcon: {
+      height: '4rem', 
+      width: '4rem', 
+    }
   }));
 
   const classes = useStyles();
@@ -39,28 +52,60 @@ export default function FooterNav() {
   const [modal, setModal] = useState(false);
   const handleModalToggle = () => {
     setModal(!modal);
-  }
+  };
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
     <div>
-        <Modal
+      <Modal
         open={modal}
         onClose={() => handleModalToggle()}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
-        keepMounted
+        style={{ width: "100vw", height: "100vh" }}
       >
-          <p>Testing modal</p>
+        {/* Bring up 3 buttons in modal, which each choice will have its own path redirect */}
+        <div
+          style={{
+            position: "relative",
+            width: "fit-content",
+            left: "80%",
+            top: "65%",
+          }}
+        >
+          <List component="nav">
+            {/* Create Story */}
+
+            <ListItem button className={classes.listItemButton}>
+              <ListItemIcon>
+                <MenuBookIcon className={classes.listItemIcon} />
+              </ListItemIcon>
+            </ListItem>
+            {/* Log Catch */}
+
+            <ListItem button className={classes.listItemButton}>
+              <ListItemIcon>
+                <RadioIcon className={classes.listItemIcon} />
+              </ListItemIcon>
+            </ListItem>
+            {/* Add Post */}
+
+            <ListItem button className={classes.listItemButton}>
+              <ListItemIcon>
+                <PostAddIcon className={classes.listItemIcon} />
+              </ListItemIcon>
+            </ListItem>
+          </List>
+        </div>
       </Modal>
-    <BottomNavigation
-      edge="start"
-      value={value}
-      onChange={handleChange}
-      className={classes.root}
-    >
+      <BottomNavigation
+        edge="start"
+        value={value}
+        onChange={handleChange}
+        className={classes.root}
+      >
         <BottomNavigationAction
           component={Link}
           label="Feed"
@@ -68,7 +113,7 @@ export default function FooterNav() {
           icon={<DynamicFeedIcon />}
           to="/feed"
         />
-      
+
         <BottomNavigationAction
           label="Post"
           value="post"
@@ -80,10 +125,10 @@ export default function FooterNav() {
           component={Link}
           label="Waters"
           value="waters"
-          icon={<MapIcon />} 
+          icon={<MapIcon />}
           to="/waters"
-          />
-    </BottomNavigation>
+        />
+      </BottomNavigation>
     </div>
   );
 }
